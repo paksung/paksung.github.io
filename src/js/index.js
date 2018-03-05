c4f6532839de6ec15136d7143d9586f4bea2b45e
paksung <437919792@qq.com>
2018-03-05 22:26:07 +0800

    update

--- src/js/index.js
+++ src/js/index.js
@@ -144,8 +144,8 @@ $.get('https://paksung.github.io/data/tagexp.json', function(res){
                 </div>`;
     });
     $('.collapse.intro').append(tags);
-    $('.collapse.work to_be_continue').before(work);
-    $('.collapse.project to_be_continue').before(project);
+    $('.collapse.work .to_be_continue').before(work);
+    $('.collapse.project .to_be_continue').before(project);
 
     //整个网页加载完毕，移除正在加载动画
     $('.loading_cover').remove();
